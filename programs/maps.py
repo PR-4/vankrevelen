9c699bef6aa24c47702e828db61d47aa5e59a9d0
Victor Resproj <victorcarreira@id.uff.br>
2023-06-26 21:14:53 -0300

    Adiciona o mapa com as batimetrias

---
+++ programs/maps.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+import pandas as pd
+import geopandas as gpd
+from eomaps import Maps
+import folium
+import matplotlib.pyplot as plt
+
+
+
+#dados de entrada
+contorno = gpd.read_file("../shp/batimetria/GEBCO_SA_bath_contours.shp")
+contorno = contorno.set_crs(epsg = "4326", inplace = True, allow_override = True)
+PR3 = pd.read_excel("../inputs/Ldagua.xlsx",header = 0 ,# index_col = 0, 
+                          usecols = ['Nome','LATITUDE_BASE_DD','LONGITUDE_BASE_DD'] )
+
+#Transforma em um geodataframe
+PR3 = gpd.GeoDataFrame(PR3,
+            geometry = 
+            gpd.points_from_xy(x=PR3.LONGITUDE_BASE_DD, y=PR3.LATITUDE_BASE_DD),
+            crs = "EPSG:4326"
+            )
+
+
+print(contorno)
+print(PR3)
+
+#Processamento
+m = Maps(crs=Maps.CRS.Mercator.GOOGLE)
+m.set_extent((-72.0, -30.0, -35.0, -5.0))
+
+#Batimetria
+m.add_gdf(contorno, column="DEPTH", legend=True)
+
+
+#Adiciona os poços como uma nova camada do mapa
+m.add_gdf(PR3, fc="r", ec="b", lw=3, legend=True)
+
+
+#ESRI
+m.add_wms.ESRI_ArcGIS.SERVICES.World_Imagery.add_layer.xyz_layer()
+
+
+#Adiciona norte e escala
+m.add_scalebar()
+m.add_compass()
+
+#saída
+plt.ylabel('Latitude')
+plt.xlabel('Longitude')
+plt.show()
